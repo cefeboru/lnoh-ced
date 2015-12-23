7135765b5eadad5cf0b665ff6e813e873d74350e
cefeboru <cefeboru@gmail.com>
2015-12-23 16:28:29 -0600

    DB schema changed
    lnoh_modulos_seccion PK change to cod_modulo,seccion,anno,semestre.

--- src/org/tempuri/test.java
+++ src/org/tempuri/test.java
@@ -1,14 +1,56 @@
 package org.tempuri;
 
-import java.rmi.RemoteException;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.SocketException;
+import org.apache.commons.net.ftp.FTP;
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPReply;
 
 public class test {
 
-	public static void main(String[] args) throws RemoteException {
+	public static void main(String[] args) throws SocketException, IOException {
 		// TODO Auto-generated method stub
-		String data = "15579969:1,15579969:2,";
-		String[] data2 = data.split(",");
-		System.out.println(data2[1]);
+		FTPClient ftp = new FTPClient();
+		
+		String remoteFile = "mirror/FreeBSD/README.TXT";
+		
+		ftp.connect("localhost",2222);
+		ftp.enterLocalPassiveMode();
+		ftp.setFileType(FTP.BINARY_FILE_TYPE);
+		
+		int reply = ftp.getReplyCode();
+		
+		if(!FTPReply.isPositiveCompletion(reply)) {
+	        ftp.disconnect();
+	        System.err.println("FTP server refused connection, code: "+ reply);
+	        System.exit(1);
+	      }
+		
+		String filename = "sitemap.sql";
+		File downloadFile2 = new File(filename);
+		OutputStream outputStream2 = new BufferedOutputStream(new FileOutputStream(filename));
+		
+		InputStream inputStream = ftp.retrieveFileStream(remoteFile);
+		
+		byte[] bytesArray = new byte[4096];
+        int bytesRead = -1;
+        
+        while ((bytesRead = inputStream.read(bytesArray)) != -1) {
+            outputStream2.write(bytesArray, 0, bytesRead);
+        }
+		
+        boolean success = ftp.completePendingCommand();
+        if (success) {
+            System.out.println("File #2 has been downloaded successfully.");
+        }
+        
+        outputStream2.close();
+        inputStream.close();
 	}
 
 }
